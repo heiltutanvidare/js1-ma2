e53fb391277691a2d1d54addc816f0a94b8b1e47
Vidar H <vusvol@icloud.com>
2020-04-03 13:08:37 +0200

    Added my answer to question 1

---
+++ vidar-håland-js1-ma2.js
@@ -0,0 +1,52 @@
+// Question 1
+// Create a function expression by assigning an anonmyous function
+// to a variable called myFunctionExpression.
+// The function should console log your name.
+
+const myFunctionExpression = function() {
+    console.log("Vidar");
+};
+
+// Question 2
+// Select the input in the HTML below by its class.
+// Add a click event listener. The callback function passed to the event
+// listener should log the sentence I was clicked.
+// You can pass the function in directly, declare it and pass it in by its name or
+// assign it to a variable and pass it in by the variable name.
+
+// My question 2 answer
+
+// Question 3
+// Select the input in the HTML below by its id.
+// Add a keydown event listener. The callback function passed to the event listener
+// should log the value of the key that was pressed.
+
+// My question 3 answer
+
+// Question 4
+
+// My question 4 answer
+
+// Question 5
+
+// My question 5 answer
+
+// Question 6
+
+// My question 6 answer
+
+// Question 7
+
+// My question 7 answer
+
+// Question 8
+
+// My question 8 answer
+
+// Question 9
+
+// My question 9 answer
+
+// Question 10
+
+// My question 10 answer
